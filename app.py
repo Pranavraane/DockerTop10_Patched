3c7caf052e7c0dcf438bfd3f727be5760b3c804f
Pranav  Rane <92846251+Pranavraane@users.noreply.github.com>
2025-10-04 17:16:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, request, render_template_string, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    name = request.args.get("name", "User")
-    template = f"Hello {name}!"
-    return render_template_string(template)
-    #patch: remove above 2 lines and add below one line, index.html file is recommented
-    #return render_template("index.html", name=name)
+@app.route('/ssti')
+def ssti():
+    name = request.args.get('name', 'User')
+    # Pass user input as template variable instead of injecting directly into template string
+    return render_template('hello.html', name=name)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
+
